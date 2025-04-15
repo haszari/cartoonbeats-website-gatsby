614a8d2760d23cff2120068b25a3315819c6a838
Rua Haszard Morris <haszari@car.local>
2025-04-15 14:50:23 +1200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Cartoon Beats Reality - lean static gatsby site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
