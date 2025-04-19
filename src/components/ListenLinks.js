8b3420faa98c8831c9afb03941b71fb58768fcf5
haszari <haszari@cartoonbeats.com>
2025-04-20 10:25:55 +1200

    render all listen link icons

--- src/components/ListenLinks.js
+++ src/components/ListenLinks.js
@@ -2,35 +2,18 @@ import * as React from "react"
 
 import "./ListenLinks.scss"
 
+// url - listen link for store
+// store - one of beatport, spotify, apple, amazon, youtube, bandcamp, 
+// TODO enforce this with TypeScript?
+function ListenIcon({ store, url }) {
+  return (<a href={url}><span className={`icon-${store}`}></span></a>);
+}
+
 function ListenLinks({ links }) {
-  let listenElements = [];
-  
-  if (links.spotify) {
-    listenElements.push( 
-      (<a key="spotify" href={links.spotify}><span className="icon-spotify"></span></a>)
-    );
-  }
-  if (links.apple) {
-    listenElements.push( 
-      (<a key="apple" href={links.apple}><span className="icon-apple"></span></a>)
-    );
-  }
-  if (links.amazon) {
-    listenElements.push( 
-      (<a key="amazon" href={links.amazon}><span className="icon-amazon"></span></a>)
-    );
-  }
-  
-  // let appleMusic = links.appleMusic ? 
-  //    (<a key="appleMusic" href={links.appleMusic}><span className="icon-apple"></span></a>) : undefined;
-  // let googlePlay = links.googlePlay ? 
-  //    (<a key="googlePlay" href={links.googlePlay}><span className="icon-google"></span></a>) : undefined;
-  // let spotify = links.spotify ? 
-  //    (<a key="spotify" href={links.spotify}><span className="icon-spotify"></span></a>) : undefined;
-  
-  // if (listenElements.length < 1) {
-  //   listenElements.push(('out soon'));
-  // }
+  let listenElements = Object.keys(links).map(store => {
+    const url = links[store];
+    return (url && <ListenIcon key={store} store={store} url={url} />);
+  });
   
   return (
     <div className="ListenLinks">
