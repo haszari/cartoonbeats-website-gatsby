04c9efdb3e729c89eded2994725935762606c7d5
haszari <haszari@cartoonbeats.com>
2025-04-23 11:01:50 +1200

    alternating slant release rows

--- src/components/releases/Release.js
+++ src/components/releases/Release.js
@@ -10,13 +10,19 @@ import "./Release.scss"
 function Release({ title, artist, coverImage, listenLinks }) {
   return (
     <div className="Release">
-      <div className="Release-info">
-        <div className="Release-info-title">{title}</div>
-        <div className="Release-info-artist">{artist}</div>
-        <ListenLinks links={listenLinks} />
-      </div>
-      <div className="Release-cover">
-        { coverImage && <GatsbyImage image={coverImage} alt={title}/> }
+      <div className="Release-slant"></div>
+      <div className="Release-content">
+        <div className="Release-info">
+          <div className="Release-info-title">{title}</div>
+          <div className="Release-info-artist">{artist}</div>
+          <ListenLinks links={listenLinks} />
+        </div>
+        <div className="Release-cover">
+          { coverImage && <GatsbyImage 
+            image={coverImage} 
+            alt={title}
+          /> }
+        </div>
       </div>
     </div>
   )
