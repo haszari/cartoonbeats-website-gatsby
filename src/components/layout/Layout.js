3b946db2f1db8bbb9bbdd99c6ae982abee5db0fa
haszari <haszari@cartoonbeats.com>
2025-05-25 13:34:18 +1200

    retweak logo alt text

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -12,7 +12,7 @@ export default function Layout({ siteTitle, children }) {
             className="SiteLogo"
             src="../../images/site-logo.png"
             placeholder="none"
-            alt="cute little mixer logo"
+            alt="mixer logo"
           />
         </Link>
           <div className="SiteTitle">{siteTitle}</div>
