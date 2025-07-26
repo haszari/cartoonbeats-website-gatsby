96a9132c82ab15efdac227e3ec0c594e0c5d979e
haszari <haszari@cartoonbeats.com>
2025-07-26 13:27:20 +1200

    shift releases content to content - separate content from presentation, consistent with blog data

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "data",
-        path: `${__dirname}/src/data/`,
+        path: `${__dirname}/content/data/`,
       },
     },
     {
