693ef4af4fd41703c225ff500ed3f94e4d684159
haszari <haszari@cartoonbeats.com>
2025-07-26 14:58:42 +1200

    basic blog index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,20 +1,37 @@
 import * as React from "react"
 
 
-import { graphql } from 'gatsby';
-import { getImage } from "gatsby-plugin-image"
+import { Link, graphql } from 'gatsby';
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import ReleaseList from '../components/releases';
 
 import "./fonts.scss"
 import "./index.scss"
-import "../fontello/css/fontawesome-musicstores.css"
 
 const BlogIndexPage = ({data}) => {
+  const posts = data.allMarkdownRemark.nodes
+  // let featuredImg = getImage(post.frontmatter.featuredImage?.childImageSharp?.gatsbyImageData) || null
+
   return (
     <Layout siteTitle={data.site.siteMetadata.title}>
-      Blog post list coming soon
+      <div>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug
+          const featuredImg = getImage(post.frontmatter.featuredImage?.childImageSharp?.gatsbyImageData) || null
+
+          return (
+            <div key={post.fields.slug}>
+            {featuredImg ? (<GatsbyImage image={featuredImg} />) : <div className="PostGrid-featuredImage-placeholder"/>}
+              <h2>
+                <Link to={post.fields.slug} itemProp="url">
+                  <span itemProp="headline">{title}</span>
+                </Link>
+              </h2>
+            </div>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
@@ -30,31 +47,20 @@ export const query = graphql`{
       title
     }
   }
-  allReleasesYaml {
+  allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
     nodes {
-      id
-      catalogueNumber
-      artist
-      title
-      cover
-      releaseDate
-      gatsbyPath(filePath: "/releases/{releasesYaml.title}")
-      listenLinks {
-        bandcamp
-        apple
-        youtube
-        spotify
-        amazon
-        beatport
-        soundcloud
+      excerpt
+      fields {
+        slug
       }
-    }
-  }
-  allFile(filter: {sourceInstanceName: {eq: "images"}}) {
-    nodes {
-      relativePath
-      childImageSharp {
-        gatsbyImageData(width: 600, placeholder: BLURRED)
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        featuredImage {
+          childImageSharp {
+            gatsbyImageData(width: 800)
+          }
+        }
       }
     }
   }
